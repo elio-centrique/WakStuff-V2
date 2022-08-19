ad534cf5c41b074c33c1a07dc128d3b5c5e99da8
Tom Garet <tom.garet.fr@gmail.com>
2022-08-19 16:16:47 +0200

    e

--- functions/functions2.js
+++ functions/functions2.js
@@ -183,10 +183,7 @@ function load_itemslist() {
           let rarity_number = jobitem['definition']['rarity'];
           let rarity = rarity_number;
           let color;
-          let loc = jobitem['definition']['loc'];
-          if(jobitem['definition']['loc'] == null) {
-          loc = "inconnue";
-        };
+
           let level = jobitem['definition']['level'];
           switch(rarity_number) {
               case 1:
@@ -214,6 +211,11 @@ function load_itemslist() {
                     name_fr = jobitem["title"]["fr"];
                     name_en = jobitem["title"]["en"];
                 }
+
+                let loc = ""
+                if(jobitem['definition']['loc'] == null) {
+                  loc = "inconnue";
+              }
 
 
                 //console.log(sorted_stats_en)
